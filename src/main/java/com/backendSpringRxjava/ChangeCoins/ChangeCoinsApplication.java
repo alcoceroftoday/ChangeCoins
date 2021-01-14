714f131a08da0cdf067ce8f38d84d44da073b84e
david alcocer <alcoceroftoday@gmail.com>
2021-01-13 22:52:18 -0500

    base spring initializr

---
+++ src/main/java/com/backendSpringRxjava/ChangeCoins/ChangeCoinsApplication.java
@@ -0,0 +1,13 @@
+package com.backendSpringRxjava.ChangeCoins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeCoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeCoinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendSpringRxjava/ChangeCoins/ChangeCoinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendSpringRxjava.ChangeCoins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeCoinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
